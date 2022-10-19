e13c41eab14021f11cfdb4e0bcafd074656aaa09
Caleb McCoy <caleb_mccoy115@yahoo.com>
2022-10-18 21:53:45 -0500

    commit tree 9b875b3d0c935aa2679cc5dce7c5031040a7ed30

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
+const link = "https://anapioficeandfire.com/api/books"
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch(link)
+  .then((res) => res.json())
+  .then((json) => renderBooks(json))
 }
 
 function renderBooks(books) {
+  
+  
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  fetchBooks()
+  }
+);
+function potato(){
+  return 'french fries'
+}
